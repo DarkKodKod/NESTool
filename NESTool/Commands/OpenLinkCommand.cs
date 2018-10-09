878e4ca68522fc12cc0caf56e85770493952d462
freinaud <f.reinaud@bigpoint.net>
2018-10-09 15:40:17 +0200

    Finished About dialog.

---
+++ NESTool/Commands/OpenLinkCommand.cs
@@ -0,0 +1,15 @@
+﻿using ArchitectureLibrary.Commands;
+using System.Diagnostics;
+
+namespace NESTool.Commands
+{
+    public class OpenLinkCommand : Command
+    {
+        public override void Execute(object parameter)
+        {
+            string url = parameter as string;
+
+            Process.Start(url);
+        }
+    }
+}
--- NESTool/ViewModels/AboutDialogViewModel.cs
+++ NESTool/ViewModels/AboutDialogViewModel.cs
@@ -1,8 +1,28 @@
 ﻿using ArchitectureLibrary.ViewModel;
+using NESTool.Commands;
+using System.Reflection;
 
 namespace NESTool.ViewModels
 {
     public class AboutDialogViewModel : ViewModel
     {
+        public OpenLinkCommand OpenLinkCommand { get; } = new OpenLinkCommand();
+
+        public string Version
+        {
+            get => _version;
+            set
+            {
+                _version = value;
+                OnPropertyChanged("Version");
+            }
+        }
+
+        private string _version = "";
+
+        public AboutDialogViewModel()
+        {
+            Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
     }
 }
