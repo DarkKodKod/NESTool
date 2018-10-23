3903befa0f4684a979e1d31019d73a8d74bfdc89
DarkKodKod <freinaud@gmail.com>
2018-10-23 02:52:48 +0200

    Now disabled images in the menu are grayscaled using a pixel shader.

---
+++ ArchitectureLibrary/FXs/GrayscaleEffect.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Media.Effects;
+using System.Windows;
+using System.Windows.Media;
+using ArchitectureLibrary.Utils;
+
+namespace ArchitectureLibrary.FXs
+{
+    public class GrayscaleEffect : ShaderEffect
+    {
+        private static PixelShader _pixelShader = new PixelShader() { UriSource = Global.MakePackUri("FXs/Shaders/GrayscaleEffect.ps") };
+
+        public GrayscaleEffect()
+        {
+            PixelShader = _pixelShader;
+
+            UpdateShaderValue(InputProperty);
+            UpdateShaderValue(DesaturationFactorProperty);
+        }
+
+        public static readonly DependencyProperty InputProperty = RegisterPixelShaderSamplerProperty("Input", typeof(GrayscaleEffect), 0);
+        public Brush Input
+        {
+            get { return (Brush)GetValue(InputProperty); }
+            set { SetValue(InputProperty, value); }
+        }
+
+        public static readonly DependencyProperty DesaturationFactorProperty = DependencyProperty.Register("DesaturationFactor", typeof(double), typeof(GrayscaleEffect), new UIPropertyMetadata(0.0, PixelShaderConstantCallback(0), CoerceDesaturationFactor));
+        public double DesaturationFactor
+        {
+            get { return (double)GetValue(DesaturationFactorProperty); }
+            set { SetValue(DesaturationFactorProperty, value); }
+        }
+
+        private static object CoerceDesaturationFactor(DependencyObject d, object value)
+        {
+            GrayscaleEffect effect = (GrayscaleEffect)d;
+            double newFactor = (double)value;
+
+            if (newFactor < 0.0 || newFactor > 1.0)
+            {
+                return effect.DesaturationFactor;
+            }
+
+            return newFactor;
+        }
+    }
+}
---
+++ ArchitectureLibrary/Utils/Global.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace ArchitectureLibrary.Utils
+{
+    public static class Global
+    {
+        /// <summary>
+        /// Helper method for generating a "pack://" URI for a given relative file based on the
+        /// assembly that this class is in.
+        /// </summary>
+        public static Uri MakePackUri(string relativeFile)
+        {
+            string uriString = $"pack://application:,,,/{AssemblyShortName};component/{relativeFile}";
+            return new Uri(uriString);
+        }
+
+        private static string AssemblyShortName
+        {
+            get
+            {
+                if (_assemblyShortName == null)
+                {
+                    Assembly a = typeof(Global).Assembly;
+
+                    // Pull out the short name.
+                    _assemblyShortName = a.ToString().Split(',')[0];
+                }
+
+                return _assemblyShortName;
+            }
+        }
+
+        private static string _assemblyShortName;
+    }
+}
