de101621c9dacd19b7b77c2965812e472b8fbb0e
DarkKodKod <freinaud@gmail.com>
2019-03-26 00:13:05 +0100

    Just an empty class to know that I have to complete that later

---
+++ NESTool/Utils/ColorQuantizer.cs
@@ -0,0 +1,22 @@
+﻿namespace NESTool.Utils
+{
+    public class ColorQuantizer
+    {
+        /*
+         var quantizer = new WuQuantizer();
+
+                        using (var bitmap = new Bitmap(filePath))
+                        {
+                            const int alphaTransparency = 0;
+                            const int alphaFader = 0;
+
+                            using (var quantized = quantizer.QuantizeImage(bitmap, alphaTransparency, alphaFader))
+                            {
+                                ProjectModel projectModel = ModelManager.Get<ProjectModel>();
+
+                                quantized.Save(Path.Combine(projectModel.ProjectPath, "gato.bmp"), ImageFormat.Bmp);
+                            }
+                        }
+                 */
+    }
+}
