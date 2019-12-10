b5615f48d9333c6f156d873df061a3ccf76dcd22
freinaud <felipe.reinaud@ubisoft.com>
2019-12-10 18:22:04 +0100

    - Fixed crash in the mainwindow in case the of a null character model.

--- NESTool/Commands/OpenBuildProjectCommand.cs
+++ NESTool/Commands/OpenBuildProjectCommand.cs
@@ -15,6 +15,12 @@ namespace NESTool.Commands
 
             object[] values = (object[])parameter;
             Window window = (Window)values[0];
+
+			if (window == null)
+			{
+				return false;
+			}
+
             string projectName = (string)values[1];
 
             if (string.IsNullOrEmpty(projectName))
